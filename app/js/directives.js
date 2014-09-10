5635ad4c5e884cb431a219c39ac93d4d65c95dc9
Steve Crockett <crockett95@gmail.com>
2014-09-10 17:58:16 -0500

    Commented some stuff out for debug, removed minified version of main.js

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,29 +1,44 @@
 'use strict';
 
-define(['angular', 'services', 'smartforms', 'sf-fields', 'jquery'], function (angular, services, smartforms, sfFields, $) {
+define(['angular', 'services', 'smartforms', 'sf-fields', 'jquery', 'simple-bar-chart', 'stacked-bar-chart'],
+	function (angular, services, smartforms, sfFields, $, simpleBarChart, stackedBarChart) {
 
-	/* Directives */
-	
-	angular.module('myApp.directives', ['myApp.services'])
-		.directive('rmcpRegForm', [function () {
-			return {
-				templateUrl: 'app/partials/reg-form.html',
-				restrict: 'C',
-				//replace: true,
-				scope: {
-					model: '=',
-					callback: '&'
-				},
-				link: function (scope, element, attrs)  {
-					scope.sfFields = sfFields;
-					scope.internalCallback = function () {
-						element.find('input[type=hidden]').each(function () {
-							scope.model[$(this).attr('name')] = $(this).val();
-						});
-						scope.callback();
+		/* Directives */
+		
+		angular.module('myApp.directives', ['myApp.services'])
+			.directive('rmcpRegForm', [function () {
+				return {
+					templateUrl: 'app/partials/reg-form.html',
+					restrict: 'C',
+					//replace: true,
+					scope: {
+						model: '=',
+						callback: '&'
+					},
+					link: function (scope, element, attrs)  {
+						scope.sfFields = sfFields;
+						scope.internalCallback = function () {
+							element.find('input[type=hidden]').each(function () {
+								scope.model[$(this).attr('name')] = $(this).val();
+							});
+							scope.callback();
+						}
+						element.on('submit', smartforms.config(scope.internalCallback).submit);
 					}
-					element.on('submit', smartforms.config(scope.internalCallback).submit);
 				}
-			}
-		}]);
-});
+			}])
+			// .directive('rmcpSimpleBar', [function () {
+			// 	return {
+			// 		template: '<div></div>',
+			// 		restrict: 'C',
+			// 		scope: {
+			// 			chartData: '=',
+			// 			unlocked: '=',
+			// 			triggerRegistration: '&'
+			// 		},
+			// 		link: function (scope, element, attrs) {
+
+			// 		}
+			// 	}
+			// }]);
+	});
