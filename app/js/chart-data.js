1a73758051d9798bbbbd839236d586b22bf88af5
Stephen Crockett <crockett95@gmail.com>
2014-09-13 19:39:41 -0500

    Dummy data for charts

--- app/js/chart-data.js
+++ app/js/chart-data.js
@@ -80,6 +80,83 @@ define([], function () {
 					callout: true
 				}		
 			]
+		},
+		rmPain: {
+			type: "stacked", 
+			name: "Resource Planning Pain Points",
+			data: [
+				{
+					shortName: "New Name",
+					isDefault: true,
+					isLocked: true,
+					group: 0,
+					sample: [{quantity: 480, title: "Total Sample", id:"total"}],
+					title: "Resource Planning Pain Points",
+					dataset: [
+						{
+							id: "holisticView",
+							title: "Lack of a combined, holistic view of both resource capacity and pipeline demand",
+							data: [241] 
+						},
+						{
+							id: "whatIf",
+							title: "Unable to run what-if scenarios",
+							data: [208]
+						},
+						{
+							id: "pipelineDemand",
+							title: "Insufficient visibility into pipeline demand",
+							data: [194]
+						},
+						{
+							id: "resourceCapacity",
+							title: "Insufficient visibility into resource capacity",
+							data: [191]
+						},			{
+							id: "demandPrioritization",
+							title: "Ineffective demand prioritization and governance process",
+							data: [190]
+						}
+					],
+					callout: false
+				},
+				{
+					shortName: "Another Name",
+					isDefault: false,
+					isLocked: true,
+					group: 0,
+					sample: [{quantity: 180, title: "Low Maturity", id: 'low'}, {quantity: 146, title:"High Maturity", id: 'high'}],
+					title: "Resource Planning Pain Points by Maturity Level",
+					dataset: [
+						{
+							id: "holisticView",
+							title: "Lack of a combined, holistic view of both resource capacity and pipeline demand",
+							data: [130,82] 
+						},
+						{
+							id: "whatIf",
+							title: "Unable to run what-if scenarios",
+							data: [94,73]
+						},
+						{
+							id: "pipelineDemand",
+							title: "Insufficient visibility into pipeline demand",
+							data: [100,59]
+						},
+						{
+							id: "resourceCapacity",
+							title: "Insufficient visibility into resource capacity",
+							data: [124,52]
+						},
+						{
+							id: "demandPrioritization",
+							title: "Ineffective demand prioritization and governance process",
+							data: [103,57]
+						}
+					],
+					callout: true
+				}		
+			]
 		}
 	}
 });
